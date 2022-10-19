93a26b66b6f2e86208e6bcd06ac496ce6fdfe800
JAVACspb <al.alentjev@yandex.ru>
2022-10-19 13:15:50 +0300

    init

---
+++ src/main/java/course/ClassicMusic.java
@@ -0,0 +1,9 @@
+package course;
+
+public class ClassicMusic implements Music{
+
+    @Override
+    public String getSong() {
+        return "Generics_Rapsody";
+    }
+}
---
+++ src/main/java/course/TestSpring.java
@@ -0,0 +1,29 @@
+package course;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+//        Music musicBean = context.getBean("musicBean",Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(musicBean);
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
+
+        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+        System.out.println(comparison);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(10000);
+        secondMusicPlayer.setVolume(5);
+
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+
+//        System.out.println(musicPlayer.getName() + " \n"  +  musicPlayer.getVolume());
+        context.close();
+    }
+}
